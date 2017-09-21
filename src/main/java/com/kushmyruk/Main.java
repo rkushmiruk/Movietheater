4d8102cca617254b9b0b12e25a0dbcb138424c5a
Roman <rKushmiruk@gmail.com>
2017-09-22 00:11:56 +0300

    Add main method for console output

--- src/main/java/com/kushmyruk/Main.java
+++ src/main/java/com/kushmyruk/Main.java
@@ -1,35 +1,53 @@
-package com.kushmyruk.dao.impl;
+package com.kushmyruk;
 
+import com.kushmyruk.dao.impl.UserDaoImpl;
+import com.kushmyruk.domain.User;
+import com.kushmyruk.service.impl.BookingServiceImpl;
+import com.kushmyruk.service.impl.UserServiceImpl;
 import com.kushmyruk.store.Store;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    @Autowired
     private static Store store;
-
+    private static UserDaoImpl userDao;
+    private static UserServiceImpl userService;
+    private static BookingServiceImpl bookingService;
     private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
-        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
-        context.load("com/kushmyruk/domain/store-context-config.xml");
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext("com/kushmyruk/domain/store-context-config.xml");
 
-        context.refresh();
+        Set<Long> set = new HashSet<>();
+        set.add(1L);
+        set.add(2L);
 
-        System.out.println(store);
-//        store.getUserStore().entrySet().forEach(LOGGER::info);
-//        store.getTicketStore().entrySet().forEach(LOGGER::info);
+        store = (Store) context.getBean("store");
+        userDao = (UserDaoImpl) context.getBean("userDaoImpl");
+        userService = (UserServiceImpl) context.getBean("userServiceImpl");
+        bookingService = (BookingServiceImpl) context.getBean("bookingServiceImpl");
+
+        User user3 = new User(4L, "Leaha", "Kushmyruk", "Ramon_2@mail.ru");
+        User user2 = new User(2L, "Mia", "Kushmyruk", "Ravioli@mail.ru");
+        User user5 = new User(5L, "Dina", "Liamel", "Linch@mail.ru");
+
+        userService.save(user5);
+        userDao.insert(user3);
+        userDao.update(user2);
+
+        userDao.findAll().forEach(LOGGER::info);
+        LOGGER.info(userService.getAll());
+        LOGGER.info(bookingService.getTicketsPrice(store.getEventStore().get(1L),
+                new Date(new java.util.Date().getTime()),
+                user2, set));
+
 
     }
 
-    @Autowired
-    public void setStore(Store store) {
-        this.store = store;
-    }
-
-    public Store getStore() {
-        return store;
-    }
 }
